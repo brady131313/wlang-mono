a36aea0ef13fc0f125c0ef55282c7606af994797
Brady Pierce <bradypierce13@gmail.com>
2023-08-20 22:28:32 -0700

    Add arbitrary to fuzz from str slice and sample corpus

---
+++ crates/wlang/examples/simple.wl
@@ -0,0 +1,7 @@
+# Bench Press
+225 x5
+275 x8
+
+# Pull Ups
+BW x8
+bw + 45 x5
--- crates/wlang/fuzz/fuzz_targets/fuzz_parser.rs
+++ crates/wlang/fuzz/fuzz_targets/fuzz_parser.rs
@@ -3,9 +3,7 @@
 use libfuzzer_sys::fuzz_target;
 use wlang::{lexer::lex, parser::parse};
 
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        let tokens = lex(s);
-        let (_tree, _errors) = parse(tokens);
-    }
+fuzz_target!(|data: &str| {
+    let tokens = lex(data);
+    let (_tree, _errors) = parse(tokens);
 });
