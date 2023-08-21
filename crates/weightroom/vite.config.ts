72b02419099194c83d55375cadf156aecf07675f
Brady Pierce <bradypierce13@gmail.com>
2023-08-20 18:17:58 -0700

    Remove all vite stuff

--- crates/weightroom/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { ViteRsw } from "vite-plugin-rsw";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [ViteRsw(), react()],
-});
