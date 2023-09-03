7bba014a043fdd1102585fb846f3e3f827cc4a15
brady pierce <bradypierce13@gmail.com>
2023-09-02 19:09:43 -0700

    show cst and hir output

--- crates/weightroom/src/App.svelte
+++ crates/weightroom/src/App.svelte
@@ -3,11 +3,14 @@
     import Card from './lib/Card.svelte'
     import Editor from './lib/Editor.svelte'
     import TokenPill from './lib/TokenPill.svelte'
-    import { type JSToken, lex } from 'wlang-web'
+    import { type JSToken, lex, WorkoutCst, WorkoutHir } from 'wlang-web'
 
     let input: string = ''
-    let tokens: JSToken[] = []
     let offset: number
+
+    let tokens: JSToken[] = []
+    let cst: WorkoutCst | null = null
+    let hir: WorkoutHir | null = null
 
     const getSuggestion = () => {
         const word = input.split(' ').pop()
@@ -15,7 +18,16 @@
         return null
     }
 
+    const SAMPLES: Record<string, string> = {
+        simple: `# Bench Press
+225 x5 
+        `,
+        complex: '',
+    }
+
     $: tokens = lex(input)
+    $: cst = new WorkoutCst(input)
+    $: if (cst) hir = new WorkoutHir(cst)
 </script>
 
 <main class="py-4">
@@ -26,21 +38,46 @@
                     Input {offset}
                 </h1>
 
-                <Editor
-                    bind:value={input}
-                    bind:cursorOffset={offset}
-                    {getSuggestion}
-                />
+                <div class="space-y-2">
+                    <div class="flex gap-2">
+                        {#each Object.keys(SAMPLES) as sample}
+                            <button
+                                class="btn btn-xs btn-secondary"
+                                on:click={() => {
+                                    input = SAMPLES[sample].trim()
+                                    offset = input.length
+                                }}
+                            >
+                                {sample}
+                            </button>
+                        {/each}
+                    </div>
+                    <Editor
+                        bind:value={input}
+                        bind:cursorOffset={offset}
+                        {getSuggestion}
+                    />
+                </div>
             </Card>
 
             <Card>
                 <h1 slot="header" class="text-lg font-medium text-gray-900">
                     Tokens
                 </h1>
-                <div class="flex gap-2">
+                <div class="flex flex-wrap gap-2">
                     {#each tokens as token}
                         <TokenPill {token} />
                     {/each}
+                </div>
+            </Card>
+
+            <Card>
+                <h1 slot="header" class="text-lg font-medium text-gray-900">
+                    Trees
+                </h1>
+                <div class="flex justify-between gap-4">
+                    <pre class="flex-1 text-gray-700">{cst?.toString()}</pre>
+                    <pre class="flex-1 text-gray-700">{hir?.toString()}</pre>
                 </div>
             </Card>
         </div>
